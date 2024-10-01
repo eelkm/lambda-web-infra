175bf002fd5ce2053100a7fb856eab173ae5a7a6
eelkm <edgarsapinis1@gmail.com>
2024-10-01 20:24:58 +0300

    Added CORS  [OPTIONS] for api gateway

--- api/api_gateway.tf
+++ api/api_gateway.tf
@@ -30,6 +30,54 @@ resource "aws_api_gateway_integration" "api_lambda_integration" {
   uri                     = aws_lambda_function.api_lambda.invoke_arn
 }
 
+resource "aws_api_gateway_method" "cors_options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.myapi.id
+  resource_id   = aws_api_gateway_resource.api_proxy_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "cors_options_integration" {
+  rest_api_id = aws_api_gateway_rest_api.myapi.id
+  resource_id = aws_api_gateway_resource.api_proxy_resource.id
+  http_method = aws_api_gateway_method.cors_options_method.http_method
+
+  type             = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_integration_response" "cors_options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.myapi.id
+  resource_id = aws_api_gateway_resource.api_proxy_resource.id
+  http_method = aws_api_gateway_method.cors_options_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'"
+    "method.response.header.Access-Control-Allow-Methods" = "'GET,POST,PUT,DELETE,OPTIONS'"
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+  }
+
+  depends_on = [aws_api_gateway_integration.cors_options_integration]
+}
+
+resource "aws_api_gateway_method_response" "cors_options_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.myapi.id
+  resource_id = aws_api_gateway_resource.api_proxy_resource.id
+  http_method = aws_api_gateway_method.cors_options_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+    "method.response.header.Access-Control-Allow-Origin"  = true
+  }
+
+  depends_on = [aws_api_gateway_method.cors_options_method]
+}
+
 resource "null_resource" "always_deploy" {
   triggers = {
     always_trigger = "${timestamp()}"
@@ -43,6 +91,9 @@ resource "aws_api_gateway_deployment" "api_deployment" {
   depends_on = [
     aws_api_gateway_integration.api_lambda_integration,
     aws_api_gateway_method.api_proxy_method,
+    aws_api_gateway_method.cors_options_method,       
+    aws_api_gateway_integration.cors_options_integration, 
+    aws_api_gateway_integration_response.cors_options_integration_response,
     null_resource.always_deploy
   ]
 
